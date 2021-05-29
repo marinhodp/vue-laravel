7cd27d4102ce4b67581cf9545c7b1e55ef8a78da
Diego Marinho <diegomarinho.dev@outlook.com>
2021-05-29 15:03:43 -0300

    Added remove / edit methods to user.

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -75,4 +75,86 @@ class UserController extends Controller
             );
         }
     }
+
+    public function remove($id)
+    {
+        $to_validate = array(
+            'id' => $id
+        );
+
+        $validation = Validator::make($to_validate, [
+            'id' => 'required|numeric|exists:users,id'
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json([
+                'message' => 'Erros de validação',
+                'errors' => $validation->errors()
+                ],
+                400
+            );
+        } else {
+            $user = User::find($id);
+            $user->forceDelete();
+            $response = 'Usuário '.$id.' removido com sucesso.';
+            return response()->json(
+                $response,
+                200
+            );
+        }
+    }
+
+    public function edit($id, Request $request)
+    {
+        $validation_error = false;
+        $validation_error_list = '';
+
+        $to_validate = array(
+            'id' => $id
+        );
+
+        $validation = Validator::make($to_validate, [
+            'id' => 'required|numeric|exists:users,id'
+        ]);
+
+        if ($validation->fails()) {
+            $validation_error = true;
+            $validation_error_list = $validation->errors();
+        } else {
+            $validation = Validator::make($request->all(), [
+                'name' => 'required|string|max:100',
+                'email' => 'required|email|unique:users,email|max:100',
+                'password' => 'required|string|min:6|max:100',
+                'phone' => 'required|numeric|digits_between:10,11'
+            ]);
+
+            if ($validation->fails()) {
+                $validation_error = true;
+                $validation_error_list = $validation->errors();
+            }
+        }
+
+        if ($validation_error) {
+            return response()->json([
+                'message' => 'Erros de validação',
+                'errors' => $validation_error_list
+                ],
+                400
+            );
+        } else {
+            $user = User::find($id);
+
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = password_hash($request->password, PASSWORD_BCRYPT);
+            $user->phone = $request->phone;
+
+            $user->save();
+
+            return response()->json(
+                $user,
+                200
+            );
+        }
+    }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -7,3 +7,5 @@ use App\Http\Controllers\UserController;
 Route::get('/user/{id}', [UserController::class, 'get']);
 Route::get('/user', [UserController::class, 'list']);
 Route::post('/user', [UserController::class, 'add']);
+Route::delete('/user/{id}', [UserController::class, 'remove']);
+Route::post('/user/{id}', [UserController::class, 'edit']);
